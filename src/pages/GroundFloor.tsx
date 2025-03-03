fc03fce62a303ce1b4525f10099fecb4192862f7
ichize <65010577+ichize@users.noreply.github.com>
2025-03-03 08:21:17 +0800

    Update GroundFloor.tsx

--- src/pages/GroundFloor.tsx
+++ src/pages/GroundFloor.tsx
@@ -28,7 +28,7 @@ const GroundFloor = () => {
                 title="Main Entrance"
                 description="The primary entrance features automatic sliding glass doors and a security checkpoint for visitors."
                 position="right"
-                modelPosition={[30, 1, 5]} 
+                modelPosition={[35, 1, 0]} 
               />
               <HoverDetails
                 x={50}
